74f545c285fabe15d360878c7b2bb20e9c0d442d
Sungmin Kim <chefminkim58@gmail.com>
2022-04-02 13:53:14 -0400

    update sign up

--- register.php
+++ register.php
@@ -9,9 +9,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
    $email = trim($_POST['email']);
    $password = trim($_POST['password']);
    $confirm_password = trim($_POST['confirm_password']);
+   $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+   if($query = $db->prepare("SELECT * FROM users WHERE email = ?")) {
+      $error = '';
+   $query->bind_param('s', $email);
+   $query->execute();
+   $query->store_result();
+      if ($query->num_rows > 0) {
+         $error .= '<p class="error">The email address is already registered!</p>';
+      } else {
+         if (strlen($password) < 6) {
+            $error .= '<p class="error">Password must have at least 6 characters</p>';
+         }
+
+         if (empty($confirm_password)) {
+            $error .='<p class="error">Please enter confirm password.</p>';
+         } else {
+            if (empty($error) && (password != $confirm_password)) {
+               $error .= '<p class="error">Password did not match.</p>';
+            }
+         }
+
+         if (empty($error)) {
+            $insertQuery = $db->prepare("INSERT INTO users (name, email, password)
+            VALUES (?, ?, ?);");
+            $insertQuery->bind_param("sss", $fullname, $email, $password_hash);
+            $result = $insertQuery->execute();
+            if ($result) {
+               $error .= '<p class="success">Your registration was successful!</p>';
+            } else {
+               $error .= '<p class="error">Something went wrong!</p>';
+            }
+         }
+      }
+   }
+   $query->close();
+   $insertQuery->close();
+   //Close DB connection
+   mysqli_close($db);
 }
-
-
+?>
 
 <!DOCTYPE html>
 <html lang="en">
