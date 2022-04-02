f45c50bdf31e94ba9a25ef50859b022f034bdddb
quorimo <60864175+quorimo@users.noreply.github.com>
2022-04-02 14:43:51 -0400

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,6 +3,11 @@ CREATE TABLE `users` (
     `name` varchar(75) NOT NULL,
     `password` varchar(255) NOT NULL,
     `email` varchar(100) NOT NULL,
+    'school' varchar(255), 
+    'address' varchar(255), 
+    'country' varchar(255),
+    'expertise' varchar(255),
+    'toLearn' varchar(255),
     PRIMARY KEY (`id`),
     UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
